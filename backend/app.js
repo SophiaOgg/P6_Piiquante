118c891845d712e8a9b1c6ddc560ddd80c9492e9
SophiaOgg <120841884+SophiaOgg@users.noreply.github.com>
2023-05-11 14:40:26 +0200

    change_thing/sauce

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const app = express();
 
 // Les routes
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-const Thing = require('./models/Things');
+const Sauce = require('./models/sauce');
 
+const path = require('path');
 
 mongoose.connect('mongodb+srv://sophia45:saucepiiquante@cluster0.dpm0pif.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
-
 
 
 app.use((req, res, next) => {
@@ -25,16 +23,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
-  app.use(express.json());
-
-
-
-app.use('/api/stuff', stuffRoutes);
+app.use(express.json());
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
-
-const path = require('path');
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
